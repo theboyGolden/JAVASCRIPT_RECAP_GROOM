16103daee646987ca636a59f238fa6c57a950944
theboyGolden <preciousstick15@gmail.com>
2024-01-09 21:08:32 +0000

    eight lesson added

---
+++ eight_lesson.js
@@ -0,0 +1,12 @@
+// Arrow functions
+
+/* Arrow functions are a concise and shorter way to define functions in JavaScript. They were introduced in ECMAsCRIPT 6 (ES6) and provide a more compact syntax compared to traditional function expressions*/
+
+function name(username) {
+  return `Hello ${username}`;
+}
+
+// to write the arrow function, we omit the reserved 'function' keyword and then write an equal sign after the function name and then after the parameter function we write an arrow 
+name = (username) => {
+  return `Hello ${username}`;
+};
