0b91828c87ea17aaeda8dd025ffaca377cbc8daa
theboyGolden <preciousstick15@gmail.com>
2024-02-18 23:30:43 +0000

    .

---
+++ find_factorial.js
@@ -0,0 +1,3 @@
+// Task: Calculate Factorial Using Recursion
+
+// Write a JavaScript function called factorial that takes a positive integer n as input and calculates the factorial of n using recursion. The factorial of a number n, denoted as n!, is the product of all positive integers less than or equal to n.
