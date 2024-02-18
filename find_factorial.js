ebcc71678d7e9d9bfe9621b443d178ff9c19f887
theboyGolden <preciousstick15@gmail.com>
2024-02-18 23:31:47 +0000

    .

--- find_factorial.js
+++ find_factorial.js
@@ -7,3 +7,7 @@ function factorial(n) {
     // Base case: factorial of 0 is 1
     if (n === 0) {
         return 1;
+    }
+    // Recursive case: factorial of n is n times factorial of (n - 1)
+    return n * factorial(n - 1);
+}
