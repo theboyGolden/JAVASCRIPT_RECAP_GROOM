0789bc8b52f20a3ac056dcb000276ef6b6251693
theboyGolden <preciousstick15@gmail.com>
2024-02-19 10:52:41 +0000

    .

---
+++ Fizzbuzz.js
@@ -0,0 +1,1 @@
+// Write a function that prints the numbers from 1 to n. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
