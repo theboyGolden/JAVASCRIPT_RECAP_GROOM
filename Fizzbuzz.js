1b9b6c84c716442eeeb5247d3dd072e8a2b30f11
theboyGolden <preciousstick15@gmail.com>
2024-02-19 10:54:03 +0000

    .

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -7,3 +7,10 @@ function fizzBuzz(n) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz");
         }
+        // Check if the number is a multiple of 3
+        else if (i % 3 === 0) {
+            console.log("Fizz");
+        }
+        // Check if the number is a multiple of 5
+        else if (i % 5 === 0) {
+            console.log("Buzz");
