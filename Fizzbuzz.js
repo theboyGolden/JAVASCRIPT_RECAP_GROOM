1d5e7b15da063fcffa7709443291ec883340b2d2
theboyGolden <preciousstick15@gmail.com>
2024-02-19 10:53:36 +0000

    .

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,1 +1,9 @@
 // Write a function that prints the numbers from 1 to n. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
+
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        // Check if the number is a multiple of both 3 and 5
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        }
